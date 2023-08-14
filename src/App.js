9321ea74b74ed301c8f086d4f15bfba1d966a27f
umeshsaravanan <umeshsaravanan13@gmail.com>
2023-08-14 21:31:17 +0530

    restricting users

--- src/App.js
+++ src/App.js
@@ -5,16 +5,19 @@ import Login from './Components/Login';
 import TodoList from './Routes/TodoList';
 import Add from './Routes/Add';
 import Notfound from './Routes/Notfound';
+import ProtectedRoutes from './Components/ProtectedRoutes';
 
 function App() {
   return (
     <BrowserRouter>
         <Routes>
             <Route path="/" element ={<Home />} />
-            <Route path="/login" element ={<Login />} />
-            <Route path="/list" element ={<TodoList />} />
-            <Route path="/add" element ={<Add />} />
-            <Route path="*" element ={<Notfound />} />
+            <Route element={<ProtectedRoutes />}>
+              <Route path="/login" element ={<Login />} />
+              <Route path="/list" element ={<TodoList />} />
+              <Route path="/add" element ={<Add />} />
+              <Route path="*" element ={<Notfound />} />
+            </Route>
         </Routes>
     </BrowserRouter>
   );
---
+++ src/Components/ProtectedRoutes.js
@@ -0,0 +1,27 @@
+import React, {useEffect, useState} from 'react'
+import { auth } from './FirebaseConfig'
+import { Outlet } from 'react-router-dom';
+import { onAuthStateChanged } from 'firebase/auth';
+import Home from '../Routes/Home';
+const ProtectedRoutes = () => {
+
+    const [isAuth, SetIsAuth] = useState(false)
+    const a = auth
+    useEffect(() => {
+      const unsubscribe = onAuthStateChanged(a, (user) => {
+        if (user) {
+          SetIsAuth(true)
+        } else {
+          SetIsAuth(false)
+        }
+      });
+  
+      return () => {
+        unsubscribe();
+      };
+    }, [a]);
+
+    return isAuth ? <Outlet /> : <Home />
+}
+
+export default ProtectedRoutes
